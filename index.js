0748164414be77a7ef1ba47c017b37f18c4f759e
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-06-16 13:37:25 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+
+function submitData (name, email) {
+    const formData = {
+        name: "Steve",
+        email: "steve@steve.com",
+      };
+
+      let message = 'Unauthorized Access';
+     
+
+
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object)
+
+      document.body.innerHTML = object[ "id" ];
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      document.body.innerHTML = message;
+      console.log(error.message);
+    });
+}
